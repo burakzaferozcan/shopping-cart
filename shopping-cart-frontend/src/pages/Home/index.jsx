7a479b1dac4b301484cb4c280365e2b7a3d098d8
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-09-20 22:07:13 +0300

    card component added.

--- shopping-cart-frontend/src/pages/Home/index.jsx
+++ shopping-cart-frontend/src/pages/Home/index.jsx
@@ -1,11 +1,73 @@
 import React from "react";
 import Header from "../../components/common/Header";
 import withRouter from "../../withRouter";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
 
 function Home() {
   return (
     <>
       <Header />
+      <Container className="mt-5">
+        <h3 className="d-flex justify-content-center align-self-center">
+          Ürün Listesi
+        </h3>
+        <Row className="mt-5">
+          <Col md={4} className="mt-5">
+            <Card>
+              <Card.Body>
+                <Card.Title>Test Ürün</Card.Title>
+                <Card.Text>Test Açıklama</Card.Text>
+                <Button variant="success">Sepete Ekle</Button>
+              </Card.Body>
+            </Card>
+          </Col>{" "}
+          <Col md={4} className="mt-5">
+            <Card>
+              <Card.Body>
+                <Card.Title>Test Ürün</Card.Title>
+                <Card.Text>Test Açıklama</Card.Text>
+                <Button variant="success">Sepete Ekle</Button>
+              </Card.Body>
+            </Card>
+          </Col>{" "}
+          <Col md={4} className="mt-5">
+            <Card>
+              <Card.Body>
+                <Card.Title>Test Ürün</Card.Title>
+                <Card.Text>Test Açıklama</Card.Text>
+                <Button variant="success">Sepete Ekle</Button>
+              </Card.Body>
+            </Card>
+          </Col>{" "}
+          <Col md={4} className="mt-5">
+            <Card>
+              <Card.Body>
+                <Card.Title>Test Ürün</Card.Title>
+                <Card.Text>Test Açıklama</Card.Text>
+                <Button variant="success">Sepete Ekle</Button>
+              </Card.Body>
+            </Card>
+          </Col>{" "}
+          <Col md={4} className="mt-5">
+            <Card>
+              <Card.Body>
+                <Card.Title>Test Ürün</Card.Title>
+                <Card.Text>Test Açıklama</Card.Text>
+                <Button variant="success">Sepete Ekle</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={4} className="mt-5">
+            <Card>
+              <Card.Body>
+                <Card.Title>Test Ürün</Card.Title>
+                <Card.Text>Test Açıklama</Card.Text>
+                <Button variant="success">Sepete Ekle</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
