5ceb2ccf90934164a4debaa8fbec5949573b4fe1
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-09-21 16:14:01 +0300

    virtual pos payment page, route and controller were created in backend

--- shopping-cart-backend/routes/api.php
+++ shopping-cart-backend/routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
-use App\Http\Controllers\api\home\indexController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::group(["prefix"=> "home","as"=>"home"], function () {
-    Route::get("",[indexController::class,"index"])->name("index");
+    Route::get("",[\App\Http\Controllers\api\home\indexController::class,"index"])->name("index");
+});
+
+Route::group(["prefix"=> "payment","as"=>"payment"], function () {
+    Route::get("",[\App\Http\Controllers\api\payment\indexController::class,"index"])->name("index");
 });
